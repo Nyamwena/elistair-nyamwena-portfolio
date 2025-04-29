3fbfad00a5dae38ad6af1cd506fbbd46cfd40b0e
Nyamwena <nyamselis@gmail.com>
2025-04-29 16:26:02 +0200

    testimonials

--- src/data/testimonialsData.ts
+++ src/data/testimonialsData.ts
@@ -7,5 +7,49 @@ export const testimonialsData = [
     company: "Reformed Church University",
     avatar: revesaiImg
   },
+  {
+    quote: "Elistair’s guidance and technical expertise were invaluable during our ERP implementation. He consistently ensured our code was clean, efficient, and maintainable, and always made time to mentor the team.",
+    name: "Bigwell Baramasimbe",
+    title: "Software Developer",
+    company: "Reformed Church University",
+    avatar: ""
+  },
+  {
+    quote: "Working with Elistair on the Student Portal was a fantastic experience—his attention to detail and thoughtful UX improvements made our lives so much easier and led to a seamless launch.",
+    name: "Lesyly Phiri",
+    title: "Software Developer",
+    company: "Reformed Church University",
+    avatar: ""
+  },
+  {
+    quote: "Elistair's leadership on our ERP project was exceptional, he not only delivered a robust, " +
+        "user-friendly system on time but also tailored every module to our unique campus workflows. " +
+        "His proactive approach and deep understanding of our needs transformed our data management, reducing manual processing by over 75%.",
+    name: "Mr. O. Munemo",
+    title: "ICTS Director",
+    company: "Gwanda State University",
+    avatar: ""
+  },
+  {
+    quote: "Elistair’s Student Portal and Online Application System completely transformed our admissions process. " +
+        "His intuitive design and attention to data integrity reduced application errors by 65% and cut processing time in half. " +
+        "He was responsive to feedback and ensured our team was fully trained—truly outstanding work.",
+    name: "Mrs L. Chinyama",
+    title: "Assistant Registrar – Admissions and Student Records",
+    company: "Reformed Church University",
+    avatar: ""
+  },
+  {
+    quote: "Elistair’s Exam & Marks Management module in the ERP has revolutionized our assessment processes. Automating grade entry, validation, " +
+        "and reporting reduced manual errors by over 90% and cut end-of-term processing from days to hours." +
+        " His tailored features and responsive support made exam administration seamless.",
+    name: "Mr. Tarwa",
+    title: "Lecturer",
+    company: "Reformed Church University",
+    avatar: ""
+  }
+
+
+
 
 ];
