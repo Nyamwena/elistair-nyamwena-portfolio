3bc50a081ea6f5c1cc2f73b37beeb3c931c4518c
Nyamwena <nyamselis@gmail.com>
2025-04-29 16:41:58 +0200

    avatar

--- src/data/testimonialsData.ts
+++ src/data/testimonialsData.ts
@@ -1,4 +1,5 @@
 import revesaiImg from '../images/revesai.png';
+import defaulAvt from '../images/defaul_avatar.png';
 export const testimonialsData = [
   {
     quote: "Elistair's ERP rollout transformed how we handle student data. He's incredibly detail-oriented and maintains clear communication throughout every project phase.",
@@ -12,14 +13,14 @@ export const testimonialsData = [
     name: "Bigwell Baramasimbe",
     title: "Software Developer",
     company: "Reformed Church University",
-    avatar: ""
+    avatar: defaulAvt
   },
   {
     quote: "Working with Elistair on the Student Portal was a fantastic experience—his attention to detail and thoughtful UX improvements made our lives so much easier and led to a seamless launch.",
     name: "Lesyly Phiri",
     title: "Software Developer",
     company: "Reformed Church University",
-    avatar: ""
+    avatar: defaulAvt
   },
   {
     quote: "Elistair's leadership on our ERP project was exceptional, he not only delivered a robust, " +
@@ -28,7 +29,7 @@ export const testimonialsData = [
     name: "Mr. O. Munemo",
     title: "ICTS Director",
     company: "Gwanda State University",
-    avatar: ""
+    avatar: defaulAvt
   },
   {
     quote: "Elistair’s Student Portal and Online Application System completely transformed our admissions process. " +
@@ -37,16 +38,16 @@ export const testimonialsData = [
     name: "Mrs L. Chinyama",
     title: "Assistant Registrar – Admissions and Student Records",
     company: "Reformed Church University",
-    avatar: ""
+    avatar: defaulAvt
   },
   {
     quote: "Elistair’s Exam & Marks Management module in the ERP has revolutionized our assessment processes. Automating grade entry, validation, " +
-        "and reporting reduced manual errors by over 90% and cut end-of-term processing from days to hours." +
+        "and reporting reduced manual errors by over 90% and cut end-of-semester processing from days to hours." +
         " His tailored features and responsive support made exam administration seamless.",
     name: "Mr. Tarwa",
     title: "Lecturer",
     company: "Reformed Church University",
-    avatar: ""
+    avatar: defaulAvt
   }
 
 
